5add6f8bda32dd2aa15c65b9aae8392f31cef7d6
ciao-madesign <ciao.madesign@gmail.com>
2026-05-01 09:15:10 +0200

    Add corpora data structure with multiple sources

---
+++ src/data/corpora.js
@@ -0,0 +1,19 @@
+export const corpora = {
+  wiki: {
+    name: "Wikipedia (IT)",
+    lang: "it",
+    url: "https://raw.githubusercontent.com/selva86/datasets/master/newsgroups.json"
+  },
+
+  subtitles: {
+    name: "OpenSubtitles (EN)",
+    lang: "en",
+    url: "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+  },
+
+  mixed: {
+    name: "Mixed Corpus",
+    lang: "multi",
+    url: "https://raw.githubusercontent.com/dscape/spell/master/test/resources/big.txt"
+  }
+};
